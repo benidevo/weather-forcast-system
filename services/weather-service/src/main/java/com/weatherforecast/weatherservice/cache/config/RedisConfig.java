06ab5fbe11512e3d5fdc8d48ab0961926724808c
Benjamin Idewor <benjaminidewor@gmail.com>
2025-04-02 08:21:32 +0200

    Fix Redis config

--- services/weather-service/src/main/java/com/weatherforecast/weatherservice/cache/config/RedisConfig.java
+++ services/weather-service/src/main/java/com/weatherforecast/weatherservice/cache/config/RedisConfig.java
@@ -1,14 +1,11 @@
 package com.weatherforecast.weatherservice.cache.config;
 
-import java.time.Duration;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
@@ -17,9 +14,6 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.weatherforecast.weatherservice.domain.WeatherData;
-
-import io.lettuce.core.ClientOptions;
-import io.lettuce.core.SocketOptions;
 
 @Configuration
 public class RedisConfig {
@@ -35,18 +29,7 @@ public class RedisConfig {
         RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
         config.setHostName(redisHost);
         config.setPort(redisPort);
-
-        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
-                .commandTimeout(Duration.ofSeconds(5))
-                .shutdownTimeout(Duration.ofSeconds(2))
-                .clientOptions(ClientOptions.builder()
-                        .socketOptions(SocketOptions.builder()
-                                .connectTimeout(Duration.ofSeconds(5))
-                                .build())
-                        .build())
-                .build();
-
-        return new LettuceConnectionFactory(config, clientConfig);
+        return new LettuceConnectionFactory(config);
     }
 
     @Bean
